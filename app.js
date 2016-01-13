65d922bccc6b0687dfc51ca175e774287ea54173
liangchengcheng <1038127753@qq.com>
2016-01-13 20:53:59 +0800

    js-path-button
    js-path-button

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lcc on 16/1/13.
+ */
+var items = document.querySelectorAll('.circle a');
+
+for(var i = 0, l = items.length; i < l; i++)
+{
+    items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+
+    items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+}
+
+document.querySelector('.menu-button').onclick = function(e)
+{
+    e.preventDefault();
+    document.querySelector('.circle').classList.toggle('open');
+}
